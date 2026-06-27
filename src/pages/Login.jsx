5c4e9225d47b5d8859f99e6a5419c66f0b3673b6
THomeAdmin <Info@thome.co.in>
2026-06-27 18:52:29 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -126,7 +126,6 @@ console.log("Access Token:", data.access_token);
     const userData = await profileResponse.json();
 
     // Save correct user
-    
     localStorage.setItem("user", JSON.stringify(userData));
 
     // Notify app
